e84cea76093502741455230b99ef60413d534d6c
Ubuntu <835943102@qq.com>
2017-08-17 15:29:53 +0800

    redux

--- entry.jsx
+++ entry.jsx
@@ -1,13 +1,19 @@
 /**
  * Created by lmy on 17-8-10.
  */
-import React, {Component} from 'react';
-import {render} from "react-dom";
+import React from 'react';
+import {render} from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './public/js/reducers/hi';
+import App from './public/js/containers/container';
 
-class HelloMessage extends React.Component {
 
-    render() {
-        return <div>Hello World!!!</div>;
-    }
-}
-render(<HelloMessage />, document.getElementById('app'));
+let store = createStore(reducer);
+
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('app')
+)
