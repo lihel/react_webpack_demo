971b47039205bcf2c4b472e0ebcebd3de0795154
Ubuntu <835943102@qq.com>
2017-08-17 16:18:26 +0800

    middleware

--- entry.jsx
+++ entry.jsx
@@ -3,13 +3,18 @@
  */
 import React from 'react';
 import {render} from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import {Router, Route, browserHistory} from 'react-router';
 import {Provider} from 'react-redux';
 import reducer from './public/js/reducers/hi';
 import App from './public/js/containers/container';
 
 
-let store = createStore(reducer);
+import middleHi from './public/js/middlewares/middle';
+
+const createMiddlewareStore=applyMiddleware(middleHi)(createStore);
+
+let store = createMiddlewareStore(reducer);
 
 render(
     <Provider store={store}>
--- public/js/containers/container.js
+++ public/js/containers/container.js
@@ -5,6 +5,7 @@ import App from '../components/App';
 import { bindActionCreators } from 'redux';
 import {connect} from 'react-redux';
 
+//action
 function changeText(){
     return {
         type:'CHANGE_TEXT'
